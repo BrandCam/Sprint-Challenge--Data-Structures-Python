8cb64d5fcc8719f5205a5f67345bd6d64f59410c
BrandCam <BrandCam>
2019-04-12 11:42:13 -0500

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,35 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    if len(self.get()) == 5:
+      self.storage[self.current] = item
+      self.current += 1
+    else:
+      self.storage[0] = self.storage[1]
+      self.storage[1] = self.storage[2]
+      self.storage[2] = self.storage[3]
+      self.storage[3] = self.storage[4]
+      self.storage[4] = item
 
   def get(self):
-    pass
+    arr = []
+    for item in self.storage:
+      if item is not None:
+        arr.append(item)
+    return arr
+
+buffer = RingBuffer(5)
+print(buffer.append('a'))
+print(buffer.append('b'))
+print(buffer.append('c'))
+print(buffer.append('d'))
+print(buffer.append('e'))
+print(buffer.get())
+print("--------")
+print(buffer.append('f'))
+print(len(buffer.storage))
+print(buffer.get())
+print('---0---')
+print(buffer.append('g'))
+print(buffer.append('h'))
+print(buffer.append('i'))
